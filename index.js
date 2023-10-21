97a3eb765dabea33dc0e34f11160b485679ce1c3
crlse42 <carlosdlt42@hotmail.com>
2023-10-21 13:24:12 -0700

    Inputed code to get inquirer to work properly

--- index.js
+++ index.js
@@ -1,34 +1,75 @@
+const fs = require('fs');
 const inquirer = require('inquirer');
 const shapeType = require('./lib/shapes.js');
+const logoPrompt = require('./lib/questions.js');
 
-inquirer
-.prompt([
-    {
-        type: "input",
-        name: "question",
-        message: "What text would you like to display?"
-    },
+const generateLogo = (logoFile) => {
+    fs.writeFile('logo.svg', logoFile, (err) =>
+        err ? console.error(err) : console.log("Success! Your logo has been created!"),
+        console.log(`Logo was saved to ${logoFile}`)
+    )
+}
 
-    {
-        type: "input",
-        name: "textColor",
-        message: "What color would you like your text to be?"
-    },
+const startInquirer = () => {
+    inquirer
+        .prompt(logoPrompt)
+        .then((answers) => {
+            generateLogo(shapeType(answers));
+        })
+        .catch(err => {
+            console.log(err);
+        });
+}
 
-    {
-        type: "input",
-        name: "shapeColor",
-        message: "What color would you like your shape to be?"
-    },
+let init = () => {
+    startInquirer();
+}
 
-    {
-        type: "list",
-        name: "shapeType",
-        message: "What shape would you like to display?",
-        choices: ["Square", "Circle", "Triangle"]
-    }
-])
-.then(answers => {
-console.log(answers.question);
-});
+init();
 
+
+
+
+//////OLD CODE//////
+
+/// Tried a few methods by myself and with tutors, but none of them worked. I'm leaving them here for reference.
+
+// function fileInfo(file, data) {
+//     fs.fileInfo(file, data, (err) =>
+//         err ? console.error(err) : console.log("Success! Your logo has been created!"),
+//         console.log(`Logo was saved to ${file}`)
+//     )
+// }
+
+
+
+// function init() {
+//     inquirer
+//     .prompt(logoPrompt)
+//         .then((answers) => {
+//             const svgInput = shapeType(answers);
+//             fileInfo("logo.svg", svgInput);
+//         })
+//         .catch(err => {
+//             console.log(err);
+//         });
+// }
+
+// init();
+
+// let shapeInfo = shapeType(answers);
+
+// fs.writeFile("Logo.svg", shapeInfo, err => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log("Congratulations, Successfully created your shape!")
+// })
+
+// // function init() {
+// //     // console.log("Welcome, please answer the following questions to create your shape!");
+
+// //     inquirer.prompt();
+// // }
+
+// // init();
